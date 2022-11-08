4b2def14cd2de6d12d36c66c7b9e481bf6590c32
javischak <martinthomas@Martins-MacBook-Air.local>
2022-11-08 14:53:57 -0800

    maxRecurringChar

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -2,11 +2,52 @@
 Given a string of text, find and return the most recurring character. 
 e.g maxRecurringChar('aabacada') // will return 'a'
 */
+//for in fastest
 
+//for in iteration
+/* function maxRecurringChar(text) {
+    let charMap = {}
+    let maxCharValue = 0
+    let maxChar = ''
 
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
 
+    for (let char in charMap) {
+        if (charMap[char] > maxCharValue) {
+            maxCharValue = charMap[char]
+            maxChar = char
+        }
+    }
+
+    return maxChar
+} */
+
+//mapping
 function maxRecurringChar(text) {
-    // Code goes here
+    let charMap = {}
+    let charArray =[]
+    let vaulesArray = []
+    let maxCharValue = 0
+
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+
+    charArray = Object.keys(charMap)
+    vaulesArray = Object.values(charMap)
+    maxCharValue = Math.max(...vaulesArray)
+
+    return charArray[vaulesArray.indexOf(maxCharValue)]
 }
 
 
