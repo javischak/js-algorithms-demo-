3283499c9548c76659e64f8eda3c74758ae6cfdd
javischak <martinthomas@Martins-MacBook-Air.local>
2022-11-08 15:14:35 -0800

    capSentence

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -4,13 +4,38 @@ return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
 */
 
+//.map() and .splice() is fastest
 
+//.forEach()
+/* function capSentence(text) {
+  let wordsArray = text.toLowerCase().split(' ')
+  let capsArray = []
 
+  wordsArray.forEach(word => {
+      capsArray.push(word[0].toUpperCase() + word.slice(1))
+  });
 
+  return capsArray.join(' ')
+} */
+
+//.map() and .slice()
+/* function capSentence(text) {
+  let wordsArray = text.toLowerCase().split(' ')
+  let capsArray = wordsArray.map(word=>{
+      return word[0].toUpperCase() + word.slice(1)
+  })
+  return capsArray.join(' ')
+} */
+
+//map() and .replace()
 function capSentence(text) {
-   // Code goes here
-}
+  let wordsArray = text.toLowerCase().split(' ')
 
+  let capsArray = wordsArray.map( word=>{
+      return  word.replace(word[0], word[0].toUpperCase())
+  })
 
+  return capsArray.join(' ')
+  }
 
 module.exports = capSentence
