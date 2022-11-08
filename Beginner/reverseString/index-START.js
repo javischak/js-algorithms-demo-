abe60d4ca5b3905ac8104f9815faf8043d61e292
javischak <martinthomas@Martins-MacBook-Air.local>
2022-11-08 13:59:19 -0800

    comment

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -6,7 +6,28 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function reverseString(text) {
-    // Code goes here
+    return text.split("").reverse().join("")
+    //return [...text].reverse().join('') 
+
+    //ES6 we can use the spread operator as shown below to tweak this solution a bit.
+  
+    //  let result = ""
+    //for (let i = text.length - 1; i >= 0; i--) {
+   //     result += text[i]
+    //}
+    //return result
+    //loop 
+
+  /*   if (text === "") {
+        return ""
+    } else {
+        return reverseString(text.substr(1)) + text[0]
+    } */
+    //recursive
+
+    return text.split("").reduce((acc, char) => char + acc, '')
+    //return [...text].reduce((acc, char) => char + acc, '')
+    //.reduce()
 }
 
 
