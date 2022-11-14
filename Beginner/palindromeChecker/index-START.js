37f741a9e55e4d954fef25559df11e8dd01020d8
javischak <martinthomas@Martins-MacBook-Air.local>
2022-11-14 09:39:29 -0800

    palindrome stuff

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -4,11 +4,37 @@ e.g palindromeChecker('racecar') // will return true
 */
 
 
-
-
+// AN INTUITIVE APPROACH. This approach is the fastest
+/* 
 function palindromeChecker(text) {
-    // Code goes here
-}
+
+    var reversedText = text.toLowerCase()
+        .split('').reverse().join('')
+
+    return text === reversedText
+} */
+
+// LOOPING THROUGH AND COMPARING CHARACTERS
+/* function palindromeChecker(text) {
+    let charArray = text.toLowerCase().split('')
+
+    let result = charArray.every((letter, index) => {
+        return letter === charArray[charArray.length - index - 1];
+    })
+
+    return result
+} */
+
+// LOOPING THROUGH AND COMPARING CHARACTERS(OPTIMIZED)
+function palindromeChecker(text) {
+    var textLen = text.length;
+    for (var i = 0; i < textLen/2; i++) {
+      if (text[i] !== text[textLen - 1 - i]) {
+          return false;
+      }
+    }
+    return true;
+   }
 
 
 
