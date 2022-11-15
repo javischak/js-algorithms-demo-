cff57ced750d30716ffad857235311afcfdd3070
javischak <martinthomas@Martins-MacBook-Air.local>
2022-11-15 10:52:23 -0800

    rangeSum

--- Beginner/IntegerReversal/index-START.js
+++ Beginner/IntegerReversal/index-START.js
@@ -5,15 +5,18 @@ ordering of that which was received. E.g
 */
 
 
+
+// Reverses a string
 function reverseString(text) {
     return [...text].reduce((acc, char) => char + acc, '')
 }
 
+// Reverses an integer
 function reverseInteger(num) {
     let reversedNumber = parseInt(reverseString(num.toString()))
+    
     return (reversedNumber * Math.sign(num))
 }
-
 
 
 module.exports = reverseInteger;
