b3acf425e61a091a4f94a21534302a31e7b89d58
javischak <martinthomas@Martins-MacBook-Air.local>
2022-11-15 09:12:40 -0800

    searchReplace

--- Beginner/searchReplace/index-START.js
+++ Beginner/searchReplace/index-START.js
@@ -9,6 +9,33 @@ function searchReplace(str, word, newWord) {
     return str.replace(regex, newWord)
 }
 
+//replace is fastest solution
+
+// USING .REPLACE()
+
+function searchReplace(str, word, newWord) {
+
+    if (word[0] === word[0].toUpperCase()) {
+        newWord = newWord[0].toUpperCase() + newWord.slice(1)
+    }
+    return str.replace(word, newWord)
+
+}
+
+
+// USING REGULAR EXPRESSIONS
+
+function searchReplace(str, word, newWord) {
+    let regex = new RegExp(word, "gi");
+
+    if (/[A-Z]/.test(word[0])) {
+
+        newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1);
+    }
+
+    return str.replace(regex, newWord)
+}
+
 
 
 module.exports = searchReplace
